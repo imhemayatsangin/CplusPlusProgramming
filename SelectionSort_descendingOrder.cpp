74ea900ab7ab9b28265fc8e1c2e3e498be5df134
Hemayatullah Sangin <hemayat.sangin@gmail.com>
2023-04-02 01:02:30 +0300

    Descending Order using Selection Sort algorithm

---
+++ SelectionSort_descendingOrder.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+        if (minIdx != i) {
+            swap(arr[i], arr[minIdx]);
+        }
+    }
+}
+
+int main() {
+    int arr[] = {3, 6, 23, 15, 12, 8, 55, 43, 57, 90, 13, 4, 25, 72, 1, 21};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Array before sorting: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    selectionSort(arr, n);
+
+    cout << "Array after sorting by descending order: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
